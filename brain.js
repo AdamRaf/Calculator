7a64949f6c7aa89c34375d5ae2ed7358e2674d3e
Adraal <adamrafli16@gmail.com>
2022-12-19 21:24:59 +0700

    DONEEE

--- brain.js
+++ brain.js
@@ -1,20 +1,36 @@
-let act = false;
+let ipt = false;
+let val = 0;
 
 function input(x) {
     document.getElementById("line").value += x;
-    act = true;
+    ipt = true;
 }
 
 function clr() {
     document.getElementById("line").value = '';
-    act = false;
+    ipt = false;
 }
 
 function clc() {
-    let a = eval(document.getElementById("line").value);
-    if (act == true){
-        document.getElementById("line").value = a;
+    let a = eval(document.getElementsByClassName("monitor equation")[0].value);
+    if (ipt == true){
+        document.getElementsByClassName("monitor total")[0].value = a;
     } else {
         alert("input something");
     }
+}
+
+function act(q) {
+    let eq = document.getElementsByClassName("monitor equation")[0].value;
+    console.log(`eq = ${eq}`);
+    let last = eq.charAt(eq.length-1);
+    console.log(`last = ${last}`);
+    let eva = eval(document.getElementsByClassName("monitor equation")[0].value);
+    if(last == "/" || last == "*" || last == "-" || last == "+"){
+        document.getElementsByClassName("monitor equation")[0].value = document.getElementsByClassName("monitor equation")[0].value.substring(0, eq.length-1);
+        document.getElementsByClassName("monitor equation")[0].value += q;
+    } else {
+        document.getElementsByClassName("monitor equation")[0].value += q;
+    }
+    document.getElementsByClassName("monitor total")[0].value = eva;
 }
